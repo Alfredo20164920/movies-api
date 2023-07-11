fb5069ee487e34d5900d1bc727544bc869328ba3
Alfredo20164920 <apaz20164920@gmail.com>
2023-07-11 17:22:32 -0600

    feat: get data from api and show titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,29 @@ import { GetServerSideProps } from 'next'
 import { Inter } from 'next/font/google'
 import { MainLayout } from '@/components/layouts'
 import { moviesApi } from '@/api';
+import {Movie, MoviesResponse} from '@/interfaces';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home(props) {
-  console.log(props)
+interface Props {
+  movies: Movie[]
+  page: string
+  baseUrl: string
+}
+
+export default function Home(props: Props) {
+  const {movies} = props;
+  console.log(movies)
   return (
     <MainLayout>
       <main>
-        <h2>Hola mundo</h2>
+        {
+          movies.map((movie, i) => (
+            <div key={i}>
+              {movie.title}
+            </div>
+          ))
+        }
       </main>
     </MainLayout>
   )
@@ -19,10 +33,12 @@ export default function Home(props) {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const {query} = ctx;
   const {page} = query;
-  const {data} = await moviesApi.get(`/movie/popular?page=${page ? page : 1}`);
+  const {data} = await moviesApi.get<MoviesResponse>(`/movie/popular?page=${page ? page : 1}`);
   return {
     props: {
-      data: data
+      movies: data?.results,
+      page: page ? page : 1,
+      baseUrl: moviesApi.defaults.baseURL,
     }
   }
 }
